3b1dc8b43895fa71e8fad1f7ce43d8b90ffcfeb4
Sal Camassa <>
2020-08-02 21:03:27 -0400

    trying again

---
+++ public/component---src-pages-contact-js-c8b16c6c2c12813c8cf0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"Cuy+":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),c=a.n(n),o=a("Wbzz"),r=a("Bl7J"),l=a("vrFN");t.default=function(){return c.a.createElement(r.a,{pageInfo:{pageName:"contact"}},c.a.createElement(l.a,{title:"Contact"}),c.a.createElement("h1",null,"Contact page"),c.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-c8b16c6c2c12813c8cf0.js.map
